3e54fd8832d0bae09f95592693c52137ed89acb7
annoh <amienza@terpmail.umd.edu>
2024-02-29 20:00:58 -0500

    added a route for about-css

--- app.py
+++ app.py
@@ -7,13 +7,20 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Annoh Mienza! ' 'This is my first HTML page.'
 
+
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+  @app.route('/about-css')
+def about():
+    return render_template('about-css.html')
+
 
 if __name__ == '__main__':
     app.run()
